c6b1049811043f86b283c9e8e2f227d578fc44f3
Nanthicha Makjinda <nanthicha.mak@gmail.com>
2022-12-08 16:27:09 +0700

    fix: calculator logic

--- term_project.srcs/sim_1/new/testCalculator.v
+++ term_project.srcs/sim_1/new/testCalculator.v
@@ -24,11 +24,11 @@ module testCalculator();
 
 reg signed [31:0] value_in;
 reg [1:0] op; 
-reg reset, return, clk;
+reg reset, return, enable, clk;
 wire signed [31:0] result;
 wire overflow;
 
-calculator c(value_in, op, reset, return, clk, result, overflow);
+calculator c(value_in, op, reset, return, enable, clk, result, overflow);
 
 always
     #10 clk=~clk;
@@ -36,7 +36,7 @@ always
 initial
 begin
     $monitor("input %d, op %b, reset %b, return %b, result %d, overflow %b", value_in, op, reset, return, result, overflow);
-    #0 clk=0; value_in=0; return=0; reset=0; op=0;
+    #0 clk=0; value_in=0; return=0; reset=0; op=0; enable=1;
     #10 value_in=10; op=0; // S = 10
     #20 value_in=13; op=0; // S = 23
     #20 return=1; // result = 23
@@ -52,6 +52,12 @@ begin
     #20 reset=0; value_in=9999; op=0; // S = 9999
     #20 value_in=10; op=0; // overflow = 1
     #20 return=1;
+    #20 reset=1; // result = 0
+    #20 reset=0; value_in=100; op=0; // S = 100
+    #20 enable=0;
+    #80 return=1; // result = 100
+    #20 return=0; enable=1; 
+    #40 return=1; // result = 300
     #20 $finish;
 end  
     
--- term_project.srcs/sources_1/new/calculator.v
+++ term_project.srcs/sources_1/new/calculator.v
@@ -25,40 +25,35 @@ module calculator(
     input [1:0] op,
     input reset,
     input return,
+    input enable,
     input clk,
     output reg signed [31:0] result,
     output wire overflow
     );
     reg state; // 0 - calculate, 1 - return result
-    reg signed [31:0] S1;
-    wire signed [31:0] S2;
-    initial 
-    begin
-        state=0;
-        S1 = 0;
-    end
+    reg signed [31:0] A;
+    wire signed [31:0] S;
+    
+    initial
+        A = 0;
     always @(posedge clk)
     begin
-        if(return)
-            state = 1;
-        if(reset)
-            begin
-                state = 0;
-                S1 = 0;
-                result = 0;
-            end
-        else
-        begin
-            if(state)
-                begin
-                    result = S2;
-                    state = 0;
-                end
-            else if (state == 0)
-                S1 = S2;
+        state = enable;
+        if(return) begin
+            result = S;
+            state = 0;
         end
-        $monitor("state %b, S1 %d, S2 %d", state, S1, S2);
+        if(reset) begin
+            state = 0;
+            A = 0;
+            result = 0;
+        end
+        else begin
+            if (state)
+                A = S;
+        end
+//        $monitor("state %b, A %d, S %d", state, A, S);
     end
-    alu alu(S1, value_in, op, S2, overflow); 
+    alu alu(A, value_in, op, S, overflow); 
 
 endmodule
